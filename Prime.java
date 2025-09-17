d05d1143b071ab8fa2abc3ca663b4da5d0387424
anurodhM01 <anurodhmishra31@gmail.com>
2025-09-17 19:33:37 +0530

    Check Prime Number

---
+++ Prime.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Prime {
+    public static void main(String[] args) {
+    int num, i, count = 0;
+    System.out.println("Enter Any Number To Check: ");
+    Scanner sc = new Scanner(System.in);
+    num = sc.nextInt();
+    for(i = 1; i <=num; i++){
+        if(num%i == 0){
+            count++;
+        }
+    }
+    if(count==2){
+        System.out.println(num + " is Prime.");
+    }
+    else{
+        System.out.println(num + " isn't Prime.");
+    }
+        // break;
+    }
+}
