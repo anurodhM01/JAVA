afb4aade1394041826ac04cab7ca7fe7e0ba20af
anurodhM01 <anurodhmishra31@gmail.com>
2025-09-18 22:41:42 +0530

    Added LinearSearch.java

---
+++ LinearSearch.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;           //
+public class LinearSearch {
+    public static void main(String[] args){
+        int arr[] = {1, 3, 5, 2, 8, 9, 7};
+        
+        System.out.println("Enter number to search: ");
+
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        boolean search =  false;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]==num){
+                System.err.println("Number found at index "+i);
+                search = true;
+                break;
+            }
+        }
+        if(!search){
+            System.out.println("Number isn't present.");
+        }               
+    }
+}
