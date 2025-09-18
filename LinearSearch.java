9bb2971105ea70b9b78528a0a8dbb743e4857eaf
anurodhM01 <anurodhmishra31@gmail.com>
2025-09-18 22:56:24 +0530

    Added LinearSearch.java

--- LinearSearch.java
+++ LinearSearch.java
@@ -2,22 +2,22 @@
 import java.util.Scanner;           //
 public class LinearSearch {
     public static void main(String[] args){
-        int arr[] = {1, 3, 5, 2, 8, 9, 7};
+        int arr[] = {1, 3, 5, 2, 8, 9, 7};  // DECLARING AN ARRAY
         
-        System.out.println("Enter number to search: ");
+        System.out.println("Enter number to search: "); // DISPLAYING MESSAGE FOR TARGET VALUE TO SEARCH IN THE ARRAY
 
         Scanner in = new Scanner(System.in);
-        int num = in.nextInt();
+        int num = in.nextInt();                           // TARGET VALUE TO SEARCH IN THE ARRAY  
         boolean search =  false;
         for(int i=0; i<arr.length; i++){
-            if(arr[i]==num){
+            if(arr[i]==num){                              // CHECKING IF NUMBER EXISTS AT ANY PLACE IN ARRAY   
                 System.err.println("Number found at index "+i);
                 search = true;
                 break;
             }
         }
         if(!search){
-            System.out.println("Number isn't present.");
+            System.out.println("Number isn't present.");// DISPLAYING MESSAGE IF NUMBER DOESN'T EXIST.
         }               
     }
 }
