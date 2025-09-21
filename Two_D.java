885924185248d301d3d62ed2652a3e2ee73d94c3
anurodhM01 <anurodhmishra31@gmail.com>
2025-09-21 22:02:52 +0530

    Basic Code for Inputting 2D Array

---
+++ Two_D.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Two_D {
+    public static void main(String[] args) {
+    
+        int m, n;
+        System.out.println("Enter the values of m and n");
+        Scanner in = new Scanner(System.in);
+        m = in.nextInt();
+        n = in.nextInt();
+    
+        int[][] arr = new int[m][n];
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                arr[i][j] = in.nextInt();
+            }
+        }
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                System.out.print(arr[i][j]+" ");
+            }System.out.println("\n");
+        }
+    }
+}
